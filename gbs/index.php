906dff11342997ee5f9e7a849762cd130e5dbb1e
ashankaushalya97 <45938393+ashankaushalya97@users.noreply.github.com>
2018-12-20 16:08:29 +0530

    Add files via upload

---
+++ gbs/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Garbage Managemnet System</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <div class="login">
+        <img src="img/icons/i3.png" alt="" class="avatar">
+            <h1>Login Here</h1>
+            <form action="">
+                <p>Username</p>
+                <input type="text" name="" id="" placeholder="Username">
+                <p>Password</p>
+                <input type="password" name="" id="" placeholder="Password">
+                <input type="submit" value="Login">
+                <a href="#">Lost your password?</a><br>
+                <a href="#">Don't have an account?</a>
+            </form>
+
+    </div><!-- login -->
+    
+</body>
+</html>
